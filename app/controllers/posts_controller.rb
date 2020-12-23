d8bdbc8a047aff8c94c02b33b3a235f781f870dc
takao888 <75712160+takao888@users.noreply.github.com>
2020-12-23 14:04:40 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,10 @@
 class PostsController < ApplicationController
   def index  # indexアクションを定義した
-    @post = Post.find(1)  # 1番目のレコードを@postに代入
+    @posts = Post.all.order(id: "DESC")#新しいメモが一番上に表示される並び順
   end
 
-  def new
-  end
-  
   def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
